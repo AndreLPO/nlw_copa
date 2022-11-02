3b7652cc7f7608b8bdb11bb30fefdc11500dc5a4
André Oliveira <andrelpo573@gmail.com>
2022-11-01 22:50:50 -0300

    palette

---
+++ lib/themes/palette.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Pallete {
+  static const Color white = Color(0xFFFFFFFF);
+  static const Color black = Color(0xFF09090A);
+  static const Color grey900 = Color(0xFF121214);
+  static const Color grey700 = Color(0xFF202024);
+  static const Color grey500 = Color(0xFF29292E);
+  static const Color grey300 = Color(0xff8D8D99);
+  static const Color yellow500 = Color(0xFFF7DD43);
+  static const Color green300 = Color(0xFF129E57);
+  static const Color green500 = Color(0xFF047C3F);
+  static const Color fontSubtitle = Color(0xFFC4C4CC);
+  static const Color inputBorder = Color(0xFF323238);
+}
